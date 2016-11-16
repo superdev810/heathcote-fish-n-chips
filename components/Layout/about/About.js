b1c5300a3da4bf221f1e0d7b212ee5a18f0f4702
Mona <mvafatabar@gmail.com>
2016-11-16 18:00:28 +0330

    Update About.js

--- components/Layout/about/About.js
+++ components/Layout/about/About.js
@@ -22,8 +22,25 @@ class About extends React.Component {
         id="#about"
         className={cx(s.about, this.props.className)}
         ref={node => (this.root = node)}
-      >
-        <h1 className={cx(s.title, this.props.className)}>About</h1>
+        style={{ backgroundImage: 'url(./images/about-us.png)'}}>
+       <div className={cx(s.content, this.props.className)}>
+          <heathcote className={cx(s.heathcote, this.props.className)}>heathcote Fish & Chips</heathcote>
+          <history className={cx(s.history, this.props.className)}>"A tradition of fish & chips since 1960"</history>
+          <business className={cx(s.business, this.props.className)}>
+
+            Family run business located in the Heathcote NSW offering best seafood in south east sydney.
+            We are located on Princes Highway - opposite Heathcote train station and that puts us close to
+            Royal National park.
+            Our seafood is made from the finest local and imported ingredients and include Fish & Chips, 
+            Seafood Basket, Grill Packs, BBQ Seafood Platters,
+            Fish Burger, Homemade Fish Cakes, Homemade Potato Scallops, 
+            freshly battered Fish Cocktails and Calamari Rings as well as variety of kids and family packages.
+            We also offer a range of freshly made Salads, Hot Snacks, 
+            Chicken Nuggets and Chicken schnitzel. 
+            Our good reputation is based on providing quality seafood, genuinely friendly service, 
+            and maintaining high food safety and hygiene standards.
+          </business>
+        </div>
       </div>
     );
   }
