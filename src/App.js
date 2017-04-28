09edcb2db20b1ec83824a1a2edd8cebfbd37e235
Ray Vafa <vafatabar@gmail.com>
2017-04-28 16:29:36 +1000

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar } from 'react-bootstrap';
+import Routes from './Routes';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Welcome to React</h2>
-    </div>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
-);
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">Heathcote Fish and Chips</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+        </Navbar>
+        <Routes />
+      </div>
+    );
+  }
+}
 
 export default App;
