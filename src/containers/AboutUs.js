5b38262081019fbab04f01126378663baaeaa883
Ray Vafa <vafatabar@gmail.com>
2017-07-07 15:08:12 +1000

    About US section

--- src/containers/AboutUs.js
+++ src/containers/AboutUs.js
@@ -3,7 +3,20 @@ import './AboutUs.css';
 
 const AboutUs = () => (
   <div className="about-us__wrap">
-    <h1 className="about-us__main-title">About Us</h1>
+    <div className="container">
+      <h1 className="about-us__main-title">About Us</h1>
+      <h2 className="about-us__sub-title">&ldquo;A tradition of classic fish & chips since 1960&rdquo;</h2>
+      <p className="about-us__description">
+        Family run business located in the Heathcote NSW offering best seafood in south east Sydney. We are located on
+        Princes Highway - opposite Heathcote train station and that puts us close to Royal National Park.
+        Our seafood is made from the finest local and imported ingredients and include Fish & Chips, Seafood Basket,
+        Grill Packs, BBQ Seafood Platters, Fish Burger, Homemade Fish Cakes, Homemade Potato Scallops,
+        freshly battered Fish Cocktails and Calamari Rings as well as variety of kids and family packages.
+        We also offer a range of freshly made Salads, Hot Snacks, Chicken Nuggets and Chicken Schnitzel.
+        Our good reputation is based on providing quality seafood, genuinely friendly service, and maintaining high food
+        safety and hygiene standards.
+      </p>
+    </div>
   </div>
 );
 
