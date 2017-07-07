7f2d32968d534e1e9ddd9a0fbf814877efa2fb46
Ray Vafa <vafatabar@gmail.com>
2017-07-08 09:56:04 +1000

    our team page

--- src/containers/OurTeam.js
+++ src/containers/OurTeam.js
@@ -3,7 +3,19 @@ import './OurTeam.css';
 
 const OurTeam = () => (
   <div className="our-team__wrap">
-    <h1 className="our-team__main-title">Our Team</h1>
+    <div className="our-team">
+      <div className="our-team__column-left">
+        <img className="our-team__image" src="/images/our-team/team-photo-1.jpg" />
+        <img className="our-team__image-small" src="/images/our-team/team-photo-3.jpg" />
+      </div>
+      <div className="our-team__column-right">
+        <div className="our-team__description-wrap">
+          <h1 className="our-team__main-title">Our Team</h1>
+          <p className="our-team__description">Everybody is at your service to offer the best experience possible.</p>
+          <img className="our-team__image" src="/images/our-team/team-photo-2.jpg" />
+        </div>
+      </div>
+    </div>
   </div>
 );
 
