502a1382066d40545af53b61a94a60f38be9b47c
Ray Vafa <vafatabar@gmail.com>
2017-07-08 14:24:42 +1000

    alt tag

--- src/containers/OurTeam.js
+++ src/containers/OurTeam.js
@@ -5,14 +5,26 @@ const OurTeam = () => (
   <div className="our-team__wrap">
     <div className="our-team">
       <div className="our-team__column-left">
-        <img className="our-team__image" src="/images/our-team/team-photo-1.jpg" />
-        <img className="our-team__image-small" src="/images/our-team/team-photo-3.jpg" />
+        <img
+          className="our-team__image"
+          src="/images/our-team/team-photo-1.jpg"
+          alt="our team"
+        />
+        <img
+          className="our-team__image-small"
+          src="/images/our-team/team-photo-3.jpg"
+          alt="our team"
+        />
       </div>
       <div className="our-team__column-right">
         <div className="our-team__description-wrap">
           <h1 className="our-team__main-title">Our Team</h1>
           <p className="our-team__description">Everybody is at your service to offer the best experience possible.</p>
-          <img className="our-team__image" src="/images/our-team/team-photo-2.jpg" />
+          <img
+            className="our-team__image"
+            src="/images/our-team/team-photo-2.jpg"
+            alt="our team"
+          />
         </div>
       </div>
     </div>
