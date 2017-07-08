bee3b330201467cbdc0fe623877f99d04e375d1b
Liwei <goldenelib_318@yahoo.com>
2017-07-08 12:16:57 +0800

    Find Us Page 0708

--- src/containers/FindUs.js
+++ src/containers/FindUs.js
@@ -1,10 +1,90 @@
-import React from 'react';
-import './FindUs.css';
+import _ from "lodash";
 
-const FindUs = () => (
-  <div className="find-us__wrap">
-    <h1 className="find-us__main-title">Find Us</h1>
-  </div>
-);
+import { default as React, Component, } from "react";
+import { withGoogleMap, GoogleMap, Marker, } from "react-google-maps";
+import "./FindUs.css";
 
-export default FindUs;
+const GettingGoogleMap = withGoogleMap(props => (
+  <GoogleMap
+    ref={props.onMapLoad}
+    defaultZoom={18}
+    defaultCenter={{ lat: -34.0875, lng: 151.0076 }}
+    onClick={props.onMapClick}
+  >
+    {props.markers.map(marker => (
+      <Marker
+        {...marker}
+        onRightClick={() => props.onMarkerRightClick(marker)}
+      />
+    ))}
+  </GoogleMap>
+));
+
+export default class FindUs extends Component {
+
+  state = {
+    markers: [{
+      position: {
+        lat: -34.0875,
+        lng: 151.0076,
+      },
+      key: `Taiwan`,
+      defaultAnimation: 2,
+    }],
+  };
+
+  handleMapLoad = this.handleMapLoad.bind(this);
+  handleMapClick = this.handleMapClick.bind(this);
+  handleMarkerRightClick = this.handleMarkerRightClick.bind(this);
+
+  handleMapLoad(map) {
+    this._mapComponent = map;
+  }
+
+  handleMapClick(event) {
+    const nextMarkers = [
+      ...this.state.markers,
+      {
+        position: event.latLng,
+        defaultAnimation: 2,
+        key: Date.now(),
+      },
+    ];
+    this.setState({
+      markers: nextMarkers,
+    });
+
+    if (nextMarkers.length === 3) {
+      this.props.toast(
+        `Right click on the marker to remove it`,
+        `Also check the code!`
+      );
+    }
+  }
+
+  handleMarkerRightClick(targetMarker) {
+    const nextMarkers = this.state.markers.filter(marker => marker !== targetMarker);
+    this.setState({
+      markers: nextMarkers,
+    });
+  }
+
+  render() {
+    return (
+      <div className="find-us__wrap">
+          <GettingGoogleMap
+            containerElement={
+              <div style={{ height: `100%` }} />
+            }
+            mapElement={
+              <div style={{ height: `100%` }} />
+            }
+            onMapLoad={this.handleMapLoad}
+            onMapClick={this.handleMapClick}
+            markers={this.state.markers}
+            onMarkerRightClick={this.handleMarkerRightClick}
+          />
+      </div>
+    );
+  }
+}
