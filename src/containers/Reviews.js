b71cfb7517c5b10208a039241c4bd07f982a42e7
johibkhan2 <johib.khan@gmail.com>
2017-07-09 09:44:47 +0530

    review html page is completed

--- src/containers/Reviews.js
+++ src/containers/Reviews.js
@@ -3,8 +3,71 @@ import './Reviews.css';
 
 const Reviews = () => (
   <div className="reviews__wrap">
-    <h1 className="reviews__main-title">Reviews</h1>
-  </div>
+
+    <h1 className="reviews__main-title">Reviews</h1>  
+<div className="header-images">
+   
+<div id="header-img" >
+<img id="lemon-cup-img" src="/images/reviews/lemon-cup.png"/> 
+<img id="umbrella-rouge-img" src="/images/reviews/umbrella-rouge.png"/>
+<img id="umbrella-creme-img" src="/images/reviews/umbrella-creme.png"/> 
+<img id="umbrella-vert-img" src="/images/reviews/umbrella-vert.png"/>
+
+<p id="ibox-p1">The best fish and chips shop in Sydney! Beautifull fresh fish and chips, never greasy and cooked to perfection at reasonable prices. The staff are always lovely and helpful. One of our favourite places to eat in the Shire.
+<h4 className="reviewer">"Sarah-Jane Ward"</h4>    
+</p>
+<p id="ibox-p2">Love the look and  the food is delicious. Staff welcoming , friendly and efficient..
+<h4 className="reviewer">"Sheila Jeffries"</h4>    
+</p>     
+</div>
+</div>
+    
+
+    
+<div className="container">
+<div className="flex1 ibox-content">
+<p>These guys do the best fish and chips in Sydney, hands down. The fish is so fresh and moist, and the batter perfectly crisp. Love the new look too.
+<h4 className="reviewer">"Richard Enright"</h4>    
+</p>
+<br/>     
+<p>The best chips and fish ever ! Highly recommend this clean, friendly and deliciously good old fashioned fish and chips!.You Deserved to be super successful!.
+<h4 className="reviewer">"Kylie Allbon"</h4>    
+</p>
+<br/>    
+<p>Nice food, good value and very friendly staff. If you have not tried this place give it a go. The seafood basket is my favourite.
+<h4 className="reviewer">"Russell Sullivan"</h4>    
+</p>    
+
+</div>
+    
+<div className="flex1 ibox-content">
+<p>A friend recommended that I try their fish cocktails ! He claimed they were the best he had ever had . Said they were clean, fresh and fluffy and that I was missing out . He was absolutly right . They were amazing and the service was friendly. I try to stop in every time i drive to Heathcote. I recommend parkink behind the IGA and walking to the shop as it can be hard to pull up on the main highway but i would recommend these to anyway passing through.
+<h4 className="reviewer">"Mitchella</h4>    
+</p>
+<br/>    
+<p>Its nice to see a business in Heathcote make their shop welcoming well done. The fish and chips at this shop is first class. Keep up the good work .  .
+<h4 className="reviewer">"Ann Skujins"</h4>    
+</p>
+
+</div>
+    
+<div className="flex1 ibox-content">
+<p>Awesome local business. Friendly, quick and yummy. Also love the renos.
+<h4 className="reviewer">"Tam Cartmill"</h4>    
+</p>
+<br/> 
+<p>Has the best fish and chips in Sydney.. !!! Actually the best I have ever had!.
+<h4 className="reviewer">"Tommy Morrison"</h4>    
+</p>
+ <br/>    
+<p>Best fish and chips in the whole of the Sutherland Shire !.
+<h4 className="reviewer">"Matt Koosy"</h4>    
+</p>    
+
+</div>  
+</div>    
+</div>
+    
 );
 
 export default Reviews;
